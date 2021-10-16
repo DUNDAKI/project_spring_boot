0b6b60bbc97c127aad23d290efbc74e665ca0b9d
Dundaki <vigjoaopaulo.com>
2021-10-16 18:06:14 -0300

    crud completo com sucesso

--- src/main/java/com/vigjoaopaulo/controller/ClienteController.java
+++ src/main/java/com/vigjoaopaulo/controller/ClienteController.java
@@ -9,20 +9,21 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.vigjoaopaulo.ClienteRepository;
 import com.vigjoaopaulo.model.Cliente;
+import com.vigjoaopaulo.repository.ClienteRepository;
 
 @RestController
 @RequestMapping
 @Service
 
 public class ClienteController {
-	
+	private Cliente getCliente;
 	@Autowired
 	private ClienteRepository clienteRepository;
 	
@@ -36,19 +37,33 @@ public class ClienteController {
 	@GetMapping("/clientes/{id}")
 	public Cliente getCliente(@PathVariable Long id) {
 		return clienteRepository.findById(id).get();
-	}
-	
+	}	
 	@PostMapping("/clientes")
 	@ResponseStatus(HttpStatus.CREATED)
 	public Cliente addCliente(@RequestBody Cliente cliente) {
 		return clienteRepository.save(cliente);
 	}
 	
-	
 	@DeleteMapping("/clientes/{id}")
 	public void deleteCliente(@PathVariable Long id) {
-		clienteRepository.deleteById(id);
+		clienteRepository.deleteById(id);		
 	}
+	
+	
+	@PutMapping("/clientes/{id}")
+	@ResponseStatus(HttpStatus.CREATED)
+	public Cliente updateCliente(@PathVariable Long id,  @RequestBody Cliente cliente) {
+		
+		Cliente newObj = clienteRepository.findById(id).get();
+		
+		newObj.setNome(cliente.getNome());
+		
+		
+		return  clienteRepository.save(newObj);
+	
+	
+	}
+	
 	
 	
 	
